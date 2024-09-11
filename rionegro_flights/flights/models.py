a521de3018e91e4e215ee63dc25a305853b0a06b
Jacoroch <jrodrigueo@eafit.edu.co>
2024-09-11 08:12:46 -0500

    commit

--- rionegro_flights/flights/models.py
+++ rionegro_flights/flights/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Flight(models.Model):
+    NATIONAL = 'Nacional'
+    INTERNATIONAL = 'Internacional'
+
+    TYPE_CHOICES = [
+        (NATIONAL, 'Nacional'),
+        (INTERNATIONAL, 'Internacional'),
+    ]
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=15, choices=TYPE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
